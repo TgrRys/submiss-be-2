6e5a884c8b48a2001f1e88367104efd947d0be5c
TgrRys <Tegarrizky237@gmail.com>
2024-03-27 15:19:43 +0700

    coment

--- src/Infrastructure/http/_test/users.test.js
+++ src/Infrastructure/http/_test/users.test.js
@@ -11,7 +11,7 @@ describe('/users endpoint', () => {
   afterEach(async () => {
     await UsersTableTestHelper.cleanTable();
   });
-  //sitegarturu
+  //sitegarturu2
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
